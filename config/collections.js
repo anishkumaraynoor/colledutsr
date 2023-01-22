ceea108998319372babc9a2edd3726bbef4d1e24
Anishkumar <anishpravitha@gmail.com>
2023-01-22 19:00:01 +0530

    Initial

---
+++ config/collections.js
@@ -0,0 +1,18 @@
+module.exports={
+    COLLEGE_COLLECTION:'college',
+
+
+
+
+
+
+    BILLTYPE_COLLECTION:'billtype',
+    USER_COLLECTION:'user',
+    FORM_COLLECTION:'form',
+    PENSION_COLLECTION:'pension',
+    ADVANCE_COLLECTION: 'advance',
+    CLOSURE_COLLECTION: 'closure',
+    TEACHER_COLLECTION: 'teacher',
+    CASHBOOK_COLLECTION: 'cashbook',
+    AI_COLLECTION: 'ai'
+}
